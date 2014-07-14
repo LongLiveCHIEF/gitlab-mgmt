bdeab21a57226f82b1b139f3d30e35e381d29b07
Brian Vanderbusch <brian@normalgravity.com>
2014-07-14 11:08:01 -0500

    promisifying node-gitlab

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ var fs = require('fs');
 var csv = require('csv');
 var config = require('./config/auth.js');
 var userListFile = process.argv[2];
+var Promise = require('bluebird');
 
 
 // Connection
-var gitlab = new (require('gitlab'))({
+var gitlab = Promise.promisifyAll(new (require('gitlab'))({
   url:   config.gitlab.url,
   token: config.gitlab.token
-});
+}));
 
-// Listing users
-gitlab.users.all(function(users) {
-  for (var i = 0; i < users.length; i++) {
-    console.log("#" + users[i].id + ": " + users[i].email + ", " + users[i].name + ", " + users[i].created_at);
-  }
-});
+
